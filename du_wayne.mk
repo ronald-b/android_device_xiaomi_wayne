7822c898ac9c6c9d463cfc6b640f8260f9e902ad
ronald-b <ronaldsantospetersen@gmail.com>
2020-07-23 16:37:00 +0000

    wayne:update

--- du_wayne.mk
+++ du_wayne.mk
@@ -28,8 +28,6 @@ $(call inherit-product, device/xiaomi/wayne/device.mk)
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
-USE_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
 
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
